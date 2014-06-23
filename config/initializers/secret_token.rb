c230bff31a8fd2e026f8eeb6ca1ad14e4ff2e723
lelith <lelith@gmx.de>
2014-06-23 22:25:37 +0200

    setup rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaternMagika::Application.config.secret_token = '37a093f0096b0165be4c385e5f496936c10889d24ad0ba53e478a634fcf22e01ee5f3c55e0a2f0fc4e62b96c4a1c30fa0ed08b82910342e9d395a860307469f2'
