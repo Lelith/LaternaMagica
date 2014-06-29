329a936b0a4311854c5b8e5770326c6a51a74d33
lelith <lelith@gmx.de>
2014-06-29 11:51:35 +0200

    encrypt password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ActiveRecord::Base
   attr_accessor :password
+  attr_accessible :username, :email, :password, :password_confirmation
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 6..20, :on => :create
+
+  before_save :encrypt_password
+  after_save :clear_password
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+    end
+  end
+
+  def clear_password
+    self.password = nil
+  end
 end
