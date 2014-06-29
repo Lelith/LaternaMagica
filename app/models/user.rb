3712ee2c615b6aa9709717c39fe7c56ce04b1a68
lelith <lelith@gmx.de>
2014-06-29 12:02:43 +0200

    auth function

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,22 @@ class User < ActiveRecord::Base
   def clear_password
     self.password = nil
   end
+
+
+  def self.authenticate(username_or_email="", login_password="")
+    if EMAIL_REGEX.match(username_or_email)
+      user = User.find_by_email(username_or_email)
+    else
+      user = User.find_by_username(username_or_email)
+    end
+    if user && user.match_password(login_password)
+      return user
+    else
+      return false
+    end
+  end
+
+  def match_password(login_password="")
+    encrypted_password == BCrypt::Engine.hash_secret(login_password, salt)
+  end
 end
