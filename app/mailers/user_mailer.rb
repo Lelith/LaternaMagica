c8a52f5321f65271b21235aa0a7f221ce1364406
lelith <lelith@gmx.de>
2014-07-06 18:32:54 +0200

    change user mail for resetting pwd

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,9 @@ class UserMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Welcome to LaternaMagica')
   end
 
-  def reset_password_email(user)
+  def reset_password_email(user,token)
     @user = user
-    @password_reset_url = 'http://localhost:3000/password_reset?' +     @user.password_reset_token
+    @password_reset_url = 'http://localhost:3000/password_reset?' + @token.token
     mail(:to => @user.email, :subject => 'Password Reset Instructions.')
   end
 end
