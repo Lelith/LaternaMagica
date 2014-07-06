fa0216e25cae374ac1b1be2dcef0d4a6224f299e
lelith <lelith@gmx.de>
2014-07-06 08:36:58 +0200

    request new password mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,10 @@ class UserMailer < ActionMailer::Base
     @url  = 'localhost:3000'
     mail(to: @user.email, subject: 'Welcome to LaternaMagica')
   end
+
+  def reset_password_email(user)
+    @user = user
+    @password_reset_url = 'http://localhost:3000/password_reset?' +     @user.password_reset_token
+    mail(:to => @user.email, :subject => 'Password Reset Instructions.')
+  end
 end
